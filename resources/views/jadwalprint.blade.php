bf0549990c65b3fceeb52202e1da5f1ed7b3be44
Adri Syahrul <adrisyahrul20@gmail.com>
2025-02-10 02:21:14 +0700

    export done

--- resources/views/jadwalprint.blade.php
+++ resources/views/jadwalprint.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mata Pelajaran</title>
+    <title>Jadwal Ujian</title>
     <style>
         body {
             font-family: sans-serif;
